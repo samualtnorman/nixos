0026de7eff2b7a4991b8bcfcaae083b4689183d5
Samual Norman <me@samual.uk>
2024-02-06 16:55:35 +0000

    format and organise nix config

--- configuration.nix
+++ configuration.nix
@@ -1,162 +1,67 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-{
-	imports =
-		[ # Include the results of the hardware scan.
-			/etc/nixos/hardware-configuration.nix
-		];
-
-	# Bootloader.
-	boot.loader.systemd-boot.enable = true;
+{ config, pkgs, ... }: {
 	boot.loader.efi.canTouchEfiVariables = true;
-
-	networking.hostName = "samual-nixos"; # Define your hostname.
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-	# Enable networking
-	networking.networkmanager.enable = true;
-
-	# Set your time zone.
-	time.timeZone = "Europe/London";
-
-	# Select internationalisation properties.
-	i18n.defaultLocale = "en_GB.UTF-8";
-
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "en_GB.UTF-8";
-		LC_IDENTIFICATION = "en_GB.UTF-8";
-		LC_MEASUREMENT = "en_GB.UTF-8";
-		LC_MONETARY = "en_GB.UTF-8";
-		LC_NAME = "en_GB.UTF-8";
-		LC_NUMERIC = "en_GB.UTF-8";
-		LC_PAPER = "en_GB.UTF-8";
-		LC_TELEPHONE = "en_GB.UTF-8";
-		LC_TIME = "en_GB.UTF-8";
-	};
-
-	# Enable the X11 windowing system.
-	services.xserver.enable = true;
-
-	# Enable the KDE Plasma Desktop Environment.
-	services.xserver.displayManager.sddm.enable = true;
-	services.xserver.desktopManager.plasma5.enable = true;
-
-	# Configure keymap in X11
-	services.xserver = {
-		layout = "gb";
-		xkbVariant = "";
-	};
-
-	# Configure console keymap
+	boot.loader.systemd-boot.enable = true;
 	console.keyMap = "uk";
-
-	# Enable CUPS to print documents.
-	services.printing.enable = true;
-
-	# Enable sound with pipewire.
-	sound.enable = true;
+	environment.systemPackages = with pkgs; [ starship ];
 	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
-
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
-	};
-
-	# Enable touchpad support (enabled default in most desktopManager).
-	# services.xserver.libinput.enable = true;
-
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.samual = {
-		isNormalUser = true;
-		description = "Samual Norman";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; [
-			firefox
-			kate
-			vscode
-			git
-			nodejs_20
-		];
-	};
-
-	# Allow unfree packages
+	i18n.defaultLocale = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_ADDRESS = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_IDENTIFICATION = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_MEASUREMENT = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_MONETARY = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_NAME = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_NUMERIC = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_PAPER = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_TELEPHONE = "en_GB.UTF-8";
+	i18n.extraLocaleSettings.LC_TIME = "en_GB.UTF-8";
+	imports = [ /etc/nixos/hardware-configuration.nix ];
+	networking.hostName = "samual-nixos";
+	networking.networkmanager.enable = true;
+	nix.gc.automatic = true;
 	nixpkgs.config.allowUnfree = true;
-
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
-	environment.systemPackages = with pkgs; [
-		starship
-	];
-
-	# Some programs need SUID wrappers, can be configured further or are
-	# started in user sessions.
-	# programs.mtr.enable = true;
-	# programs.gnupg.agent = {
-	#   enable = true;
-	#   enableSSHSupport = true;
-	# };
-
-	# List services that you want to enable:
-
-	# Enable the OpenSSH daemon.
-	# services.openssh.enable = true;
-
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	# networking.firewall.enable = false;
-
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "23.11"; # Did you read the comment?
-
-	services.xserver.videoDrivers = [ "nvidia" ];
-	programs.zsh.enable = true;
-	users.defaultUserShell = pkgs.zsh;
-	programs.zsh.ohMyZsh.enable = true;
+	programs.git.enable = true;
+	programs.neovim.defaultEditor = true;
+	programs.neovim.enable = true;
+	programs.neovim.viAlias = true;
+	programs.neovim.vimAlias = true;
 	programs.zsh.autosuggestions.enable = true;
-	programs.zsh.syntaxHighlighting.enable = true;
 	programs.zsh.autosuggestions.strategy = [ "completion" "match_prev_cmd" "history" ];
+	programs.zsh.enable = true;
 	programs.zsh.enableBashCompletion = true;
-
-	programs.zsh.ohMyZsh.plugins = [
-		"alias-finder" "aliases" "catimg" "colored-man-pages" "colorize" "common-aliases" "cp" "dircycle" "git"
-		"git-auto-fetch" "history" "history-substring-search" "last-working-dir" "man" "qrcode" "rsync" "starship"
-	];
-
+	programs.zsh.ohMyZsh.enable = true;
+	programs.zsh.syntaxHighlighting.enable = true;
+	security.rtkit.enable = true;
 	services.openssh.enable = true;
+	services.pipewire.alsa.enable = true;
+	services.pipewire.alsa.support32Bit = true;
+	services.pipewire.enable = true;
+	services.pipewire.pulse.enable = true;
+	services.printing.enable = true;
+	services.vscode-server.enable = true;
+	services.xserver.desktopManager.plasma5.enable = true;
+	services.xserver.displayManager.sddm.enable = true;
+	services.xserver.enable = true;
+	services.xserver.layout = "gb";
+	services.xserver.videoDrivers = [ "nvidia" ];
+	services.xserver.xkbVariant = "";
+	sound.enable = true;
+	system.autoUpgrade.allowReboot = true;
+	system.autoUpgrade.enable = true;
+	system.stateVersion = "23.11";
+	time.timeZone = "Europe/London";
+	users.defaultUserShell = pkgs.zsh;
+	users.users.samual.description = "Samual Norman";
+	users.users.samual.extraGroups = [ "networkmanager" "wheel" ];
+	users.users.samual.isNormalUser = true;
+	users.users.samual.packages = with pkgs; [ firefox kate vscode nodejs_20];
 
 	programs.zsh.interactiveShellInit = ''
 		export PNPM_HOME=~/.local/share/pnpm
 		export PATH=$PNPM_HOME:$PATH
 	'';
 
-	programs.neovim.enable = true;
-	programs.neovim.defaultEditor = true;
-	programs.neovim.vimAlias = true;
-	programs.neovim.viAlias = true;
-	system.autoUpgrade.enable = true;
-	system.autoUpgrade.allowReboot = true;
-	nix.gc.automatic = true;
+	programs.zsh.ohMyZsh.plugins = [
+		"alias-finder" "aliases" "catimg" "colored-man-pages" "colorize" "common-aliases" "cp" "dircycle" "git"
+		"git-auto-fetch" "history" "history-substring-search" "last-working-dir" "man" "qrcode" "rsync" "starship"
+	];
 }

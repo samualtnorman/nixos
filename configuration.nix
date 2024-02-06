227bec83c7005713ab808eb6e587ef147a7752cc
Samual <me@samual.uk>
2024-02-06 22:45:31 +0000

    use `/etc/nixos/configuration.nix` as base add optional non version controlled `custom.nix` which extends version controlled `configuration.nix` merge in home pc nixos config into repo

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,10 @@
-{ config, pkgs, ... }: {
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.systemd-boot.enable = true;
-	console.keyMap = "uk";
-	environment.systemPackages = with pkgs; [ starship ];
-	hardware.pulseaudio.enable = false;
-	i18n.defaultLocale = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_ADDRESS = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_IDENTIFICATION = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_MEASUREMENT = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_MONETARY = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_NAME = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_NUMERIC = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_PAPER = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_TELEPHONE = "en_GB.UTF-8";
-	i18n.extraLocaleSettings.LC_TIME = "en_GB.UTF-8";
-	imports = [ /etc/nixos/hardware-configuration.nix ];
-	networking.hostName = "samual-nixos";
-	networking.networkmanager.enable = true;
+{ config, pkgs, lib, ... }: {
+	imports = [ /etc/nixos/configuration.nix ];
+	networking.hostName = lib.mkForce "samual-nixos";
 	nix.gc.automatic = true;
-	nixpkgs.config.allowUnfree = true;
 	programs.git.enable = true;
-	programs.neovim.defaultEditor = true;
 	programs.neovim.enable = true;
+	programs.neovim.defaultEditor = true;
 	programs.neovim.viAlias = true;
 	programs.neovim.vimAlias = true;
 	programs.zsh.autosuggestions.enable = true;
@@ -30,30 +13,20 @@
 	programs.zsh.enableBashCompletion = true;
 	programs.zsh.ohMyZsh.enable = true;
 	programs.zsh.syntaxHighlighting.enable = true;
-	security.rtkit.enable = true;
 	services.openssh.enable = true;
-	services.pipewire.alsa.enable = true;
-	services.pipewire.alsa.support32Bit = true;
-	services.pipewire.enable = true;
-	services.pipewire.pulse.enable = true;
-	services.printing.enable = true;
-	services.vscode-server.enable = true;
-	services.xserver.desktopManager.plasma5.enable = true;
-	services.xserver.displayManager.sddm.enable = true;
-	services.xserver.enable = true;
-	services.xserver.layout = "gb";
-	services.xserver.videoDrivers = [ "nvidia" ];
-	services.xserver.xkbVariant = "";
-	sound.enable = true;
 	system.autoUpgrade.allowReboot = true;
 	system.autoUpgrade.enable = true;
-	system.stateVersion = "23.11";
-	time.timeZone = "Europe/London";
 	users.defaultUserShell = pkgs.zsh;
-	users.users.samual.description = "Samual Norman";
-	users.users.samual.extraGroups = [ "networkmanager" "wheel" ];
-	users.users.samual.isNormalUser = true;
-	users.users.samual.packages = with pkgs; [ firefox kate vscode nodejs_20];
+	environment.shellAliases.tfa = ''tmux attach-session -t "$(tmux list-sessions | fzf | cut -d: -f1)"'';
+	environment.shellAliases."nix-shell" = "nix-shell --run zsh";
+	virtualisation.docker.enable = true;
+	programs.partition-manager.enable = true;
+	programs.kdeconnect.enable = true;
+	programs.tmux.enable = true;
+	programs.tmux.newSession = true;
+	boot.binfmt.registrations.wasm.magicOrExtension = "\\x00asm";
+	boot.binfmt.registrations.wasm.mask = "\\xff\\xff\\xff\\xff";
+	boot.binfmt.registrations.wasm.interpreter = "/run/current-system/sw/bin/wasmer";
 
 	programs.zsh.interactiveShellInit = ''
 		export PNPM_HOME=~/.local/share/pnpm
@@ -63,5 +36,13 @@
 	programs.zsh.ohMyZsh.plugins = [
 		"alias-finder" "aliases" "catimg" "colored-man-pages" "colorize" "common-aliases" "cp" "dircycle" "git"
 		"git-auto-fetch" "history" "history-substring-search" "last-working-dir" "man" "qrcode" "rsync" "starship"
+	];
+
+	environment.systemPackages = with pkgs; [
+		starship bat deno remarshal gnumake distrobox wget trash-cli fzf wabt wasmer
+	];
+	
+	users.users.samual.packages = with pkgs; [
+		firefox nodejs_20 google-chrome krita direnv gnupg wineWowPackages.stable libsForQt5.filelight xorg.xkill
 	];
 }
--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -ex
-sudo nixos-rebuild switch --upgrade-all -I nixos-config=configuration.nix
+
+if [ -e custom.nix ]
+then
+	configFile=custom.nix
+else
+	configFile=configuration.nix
+fi
+
+sudo nixos-rebuild switch --upgrade-all -I nixos-config=$configFile

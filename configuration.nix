b5d615308322baf4ac8fc25deadbf55b1f04ebf6
Samual Norman <me@samual.uk>
2024-02-06 15:36:22 +0000

    add nvidia driver

--- configuration.nix
+++ configuration.nix
@@ -132,4 +132,5 @@
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 	system.stateVersion = "23.11"; # Did you read the comment?
 
+	services.xserver.videoDrivers = [ "nvidia" ];
 }

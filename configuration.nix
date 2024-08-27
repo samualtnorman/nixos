fc3bf00e0f63a3784f265280cc4e550c410c21b7
Samual Norman <me@samual.uk>
2024-08-27 11:31:01 +0100

    upgrade Node.js 20 to 22

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,7 @@ let unstable = import <nixos-unstable> { config.allowUnfree = true; }; in
 	];
 
 	environment.systemPackages = with pkgs; [
-		starship bat deno remarshal gnumake distrobox wget trash-cli fzf wabt wasmer file lzip unstable.atuin nodejs_20
+		starship bat deno remarshal gnumake distrobox wget trash-cli fzf wabt wasmer file lzip unstable.atuin nodejs_22
 		htop fastfetch tldr gron p7zip
 	];
 	

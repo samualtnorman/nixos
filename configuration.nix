2464d7d4309c3734a7999fb9bf87d2e89632ee9f
Samual Norman <me@samual.uk>
2025-10-16 11:42:01 +0100

    add tools for looking at btrfs and hard drives. also add glow for reading markdown in terminal

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,6 @@ let unstable = import <nixos-unstable> { config.allowUnfree = true; }; in
 	programs.nix-ld.enable = true;
 
 	users.users.samual.packages = with pkgs; [
-		gnupg unzip gcc ripgrep cargo rust-analyzer python3 nodejs_24 btrfs-progs pnpm
+		gnupg unzip gcc ripgrep cargo rust-analyzer python3 nodejs_24 btrfs-progs pnpm btrfs-progs smartmontools glow
 	];
 }

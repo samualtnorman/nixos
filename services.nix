3089c55b601f3c8ef0989575ea42ba6316ae0437
Samual Norman <me@samual.uk>
2026-03-30 00:46:41 +0100

    pin last version of Kubo that worked

--- services.nix
+++ services.nix
@@ -1,8 +1,19 @@
-let unstable = import <nixos-unstable> { config.allowUnfree = true; }; in
-{ config, ... }: {
+{ config, ... }:
+
+let inherit (import <nixpkgs> {}) fetchFromGitHub; in
+
+let fetchNixpkgs =
+  { rev, sha256 ? "" }: import (fetchFromGitHub { owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {}; in
+
+let inherit (fetchNixpkgs {
+  rev = "9d0c880cb3c00ccc7c792efa8acb0a2c2d14e4c8";
+  sha256 = "Au8HuP4Q380EvBcVNrLiYnE1POyG2e4KRc8N52EvsO8=";
+}) kubo; in
+
+{
 	services.openssh.enable = true;
 	services.kubo.enable = true;
 	services.kubo.settings.Addresses.API = "/ip4/127.0.0.1/tcp/5001";
-	services.kubo.package = unstable.kubo;
+	services.kubo.package = kubo;
 	users.users.samual.extraGroups = [ config.services.kubo.group ];
 }
